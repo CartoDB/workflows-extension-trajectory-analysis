dbfef229171c582d63772015f670cf362e3f52ca
giuliacarella <giulia@carto.com>
2025-04-08 17:40:36 +0200

    added components

---
+++ components/group_trajectories/doc/README.md
@@ -0,0 +1,5 @@
+# GroupTrajectories
+
+## Description
+
+This component can be used to convert trajectory data in flat format (indexed by a trajectory identifier, a longitude, a latitude, and a timestamp) into a format indexed by the trajectory identifier.
---
+++ components/group_trajectories/src/fullrun.sql
@@ -0,0 +1,24 @@
+EXECUTE IMMEDIATE FORMAT(
+    '''
+    CREATE OR REPLACE TABLE `%s`
+    OPTIONS (
+        expiration_timestamp = TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL 30 DAY)
+    )
+    AS (
+        SELECT %s AS traj_id, 
+        ARRAY_AGG(STRUCT(%s AS lon, %s AS lat, %s AS t)) AS tpoints,
+        %s AS traj_properties
+        FROM `%s`
+        GROUP BY %s, %s
+    )
+    ''',
+    REPLACE(output_table, '`', ''),
+    input_traj_id_column,
+    input_lon_column,
+    input_lat_column,
+    input_t_column,
+    input_properties_column,
+    REPLACE(input_table, '`', ''),
+    input_traj_id_column,
+    input_properties_column
+);
