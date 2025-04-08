a255ea3f1bd35acaaf118583544ef470ec720bbe
Diego Vicente <dvicente@carto.com>
2025-04-08 20:26:56 +0200

    Include segments in the stop detector

--- components/stopdetector/src/dryrun.sql
+++ components/stopdetector/src/dryrun.sql
@@ -1,15 +1,30 @@
-EXECUTE IMMEDIATE FORMAT(
-    '''
-    CREATE OR REPLACE TABLE
-        %s
-    (
-        traj_id STRING,
-        stop_id STRING,
-        geom GEOGRAPHY,
-        start_time TIMESTAMP,
-        end_time TIMESTAMP,
-        duration_s FLOAT64
+IF method = 'Points' THEN
+    EXECUTE IMMEDIATE FORMAT(
+        '''
+        CREATE OR REPLACE TABLE
+            `%s`
+        (
+            traj_id STRING,
+            stop_id STRING,
+            geom GEOGRAPHY,
+            start_time TIMESTAMP,
+            end_time TIMESTAMP,
+            duration_s FLOAT64
+        );
+        ''',
+        REPLACE(output_table, '`', '')
     );
-    ''',
-    output_table
-);
+ELSEIF method = 'Segments' THEN
+    EXECUTE IMMEDIATE FORMAT(
+        '''
+        CREATE OR REPLACE TABLE
+            `%s`
+        (
+            traj_id STRING,
+            stop_id STRING,
+            tpoints ARRAY<STRUCT<lon FLOAT64, lat FLOAT64, t TIMESTAMP>>
+        );
+        ''',
+        REPLACE(output_table, '`', '')
+    );
+END IF;
--- components/stopdetector/src/fullrun.sql
+++ components/stopdetector/src/fullrun.sql
@@ -1,30 +1,68 @@
-EXECUTE IMMEDIATE FORMAT(
-    '''
-    CREATE OR REPLACE TABLE
-        %s
-    AS
-        SELECT
-            traj_id,
-            s.stop_id,
-            ST_GEOGFROMTEXT(s.geometry) AS geom,
-            s.start_time,
-            s.end_time,
-            s.duration_s
-        FROM %s,
-        UNNEST(
-            @@workflows_temp@@.TRAJECTORY_STOP_DETECTOR(
-                %s,
-                %s,
-                %f,
-                %f, %f, %f, %f
-            )
-        ) AS s
-        ORDER BY traj_id, s.stop_id
-    ''',
-    output_table,
-    input_table,
-    traj_id_col,
-    tpoints_col,
-    max_diameter,
-    min_duration_sec, min_duration_min, min_duration_hour, min_duration_day
-);
+IF method = "Points" THEN
+    EXECUTE IMMEDIATE FORMAT(
+        '''
+        CREATE OR REPLACE TABLE
+            `%s`
+        AS
+            SELECT
+                traj_id,
+                s.stop_id,
+                ST_GEOGFROMTEXT(s.geometry) AS geom,
+                s.start_time,
+                s.end_time,
+                s.duration_s
+            FROM `%s`,
+            UNNEST(
+                @@workflows_temp@@.TRAJECTORY_STOP_POINTS(
+                    %s,
+                    %s,
+                    %f,
+                    %f, %f, %f, %f
+                )
+            ) AS s
+            ORDER BY traj_id, s.stop_id
+        ''',
+        REPLACE(output_table, '`', ''),
+        REPLACE(input_table, '`', ''),
+        traj_id_col,
+        tpoints_col,
+        max_diameter,
+        min_duration_sec, min_duration_min, min_duration_hour, min_duration_day
+    );
+ELSEIF method = 'Segments' THEN
+    EXECUTE IMMEDIATE FORMAT(
+        '''
+        CREATE OR REPLACE TABLE
+            `%s`
+        AS
+            SELECT
+                traj_id,
+                s.stop_id,
+                ARRAY_AGG(
+                    STRUCT(
+                      s.lon AS lon,
+                      s.lat AS lat,
+                      TIMESTAMP(s.t) AS t
+                    )
+                    ORDER BY t
+                ) AS tpoints
+            FROM `%s`,
+            UNNEST(
+                @@workflows_temp@@.TRAJECTORY_STOP_SEGMENTS(
+                    %s,
+                    %s,
+                    %f,
+                    %f, %f, %f, %f
+                )
+            ) AS s
+            GROUP BY traj_id, s.stop_id
+            ORDER BY traj_id, s.stop_id
+        ''',
+        REPLACE(output_table, '`', ''),
+        REPLACE(input_table, '`', ''),
+        traj_id_col,
+        tpoints_col,
+        max_diameter,
+        min_duration_sec, min_duration_min, min_duration_hour, min_duration_day
+    );
+END IF;
