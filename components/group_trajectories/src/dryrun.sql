858a6878978189d839715e91a96ae6649ce39f3f
Giulia Carella <42603387+giuliacarella@users.noreply.github.com>
2025-04-09 11:46:54 +0200

    Added group/ungroup components (#1)

---
+++ components/group_trajectories/src/dryrun.sql
@@ -0,0 +1,23 @@
+EXECUTE IMMEDIATE FORMAT(
+    '''
+    CREATE OR REPLACE TABLE `%s`
+    OPTIONS (
+        expiration_timestamp = TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL 30 DAY)
+    )
+    AS (
+        SELECT %s AS traj_id, 
+        ARRAY_AGG(STRUCT(%s AS lon, %s AS lat, %s AS t, %s AS traj_properties)) AS tpoints,
+        FROM `%s`
+        WHERE 1 = 0
+        GROUP BY %s
+    )
+    ''',
+    REPLACE(output_table, '`', ''),
+    input_traj_id_column,
+    input_lon_column,
+    input_lat_column,
+    input_t_column,
+    input_properties_column,
+    REPLACE(input_table, '`', ''),
+    input_traj_id_column
+);
---
+++ components/ungroup_trajectories/src/fullrun.sql
@@ -0,0 +1,23 @@
+EXECUTE IMMEDIATE FORMAT(
+    '''
+    CREATE OR REPLACE TABLE `%s`
+    OPTIONS (
+        expiration_timestamp = TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL 30 DAY)
+    )
+    AS (
+        WITH CTE AS(
+            SELECT *
+            FROM `%s`
+        )
+        SELECT 
+        traj_id AS traj_id, 
+        tpoint.lon AS lon, 
+        tpoint.lat AS lat, 
+        tpoint.t AS t,
+        traj_properties AS traj_properties
+        FROM CTE, UNNEST(tpoints) AS tpoint
+    )
+    ''',
+    REPLACE(output_table, '`', ''),
+    REPLACE(input_table, '`', '')
+);
