6d3fc741812189d020e7c95c9f4621b67d0bd9e6
luciagds <100363320@alumnos.uc3m.es>
2025-05-07 14:16:49 +0200

    fix trajectorysplitter component

--- functions/trajectorysplitter.sql
+++ functions/trajectorysplitter.sql
@@ -143,8 +143,7 @@ CREATE OR REPLACE FUNCTION
     min_duration_min FLOAT64,
     min_duration_hour FLOAT64,
     min_duration_day FLOAT64,
-    min_length FLOAT64,
-    input_split_metric_column STRING
+    min_length FLOAT64
 )
 RETURNS ARRAY<STRUCT<seg_id STRING, lon FLOAT64, lat FLOAT64, t TIMESTAMP, properties STRING>>
 LANGUAGE python
@@ -170,8 +169,7 @@ def main(
     min_duration_min,
     min_duration_hour,
     min_duration_day,
-    min_length,
-    input_split_metric_column
+    min_length
 ):
     # build the DataFrame
     df = pd.DataFrame.from_records(trajectory)
@@ -208,16 +206,6 @@ def main(
         result['lon'] = result.geometry.x.astype(np.float64)
         result['lat'] = result.geometry.y.astype(np.float64)
         result['seg_id'] = result.traj_id
-
-        def update_properties(row):
-            try:
-                props = json.loads(row['properties'])
-            except json.JSONDecodeError:
-                props = {}
-            props[input_split_metric_column] = row['speed']
-            return json.dumps(props)
-
-        result['properties'] = result.apply(update_properties, axis=1)
         result = result.drop(columns=['traj_id', 'geometry','speed'])
         return result.to_dict(orient='records')
 """;
@@ -327,10 +315,14 @@ def main(traj_id, trajectory, valuechange_col, min_length):
     df['properties_dict'] = df['properties'].apply(json.loads)
     df[valuechange_col] = df['properties_dict'].apply(lambda x: x.get(valuechange_col))
 
+    # check that valuechange_col is in properties
+    if len(df.dropna()) == 0:
+        raise ValueError(f'Specified value-change column {valuechange_col} not found in properties')
+
     # build the GeoDataFrame
     gdf = (
       gpd.GeoDataFrame(
-        df[['t', valuechange_col]],
+        df[['t', 'properties', valuechange_col]],
         geometry=gpd.points_from_xy(df.lon, df.lat),
         crs=4326
       )
@@ -342,6 +334,66 @@ def main(traj_id, trajectory, valuechange_col, min_length):
 
     result = mpd.ValueChangeSplitter(traj).split(
         col_name=valuechange_col,
+        min_length=min_length
+    )
+
+    if len(result) == 0:
+        df['seg_id'] = traj_id
+        return df.to_dict(orient='records')
+    else:
+        result = result.to_point_gdf().reset_index()
+        result['lon'] = result.geometry.x.astype(np.float64)
+        result['lat'] = result.geometry.y.astype(np.float64)
+        result['seg_id'] = result.traj_id
+        result = result.drop(columns=['traj_id', 'geometry'])
+        return result.to_dict(orient='records')
+""";
+
+CREATE OR REPLACE FUNCTION
+    @@workflows_temp@@.`TRAJECTORY_ANGLECHANGE_SPLITTER`
+(
+    traj_id STRING,
+    trajectory ARRAY<STRUCT<lon FLOAT64, lat FLOAT64, t TIMESTAMP, properties STRING>>,
+    min_angle FLOAT64,
+    min_speed FLOAT64,
+    min_length FLOAT64
+)
+RETURNS ARRAY<STRUCT<seg_id STRING, lon FLOAT64, lat FLOAT64, t TIMESTAMP, properties STRING>>
+LANGUAGE python
+OPTIONS (
+    entry_point='main',
+    runtime_version='python-3.11',
+    packages=['numpy','pandas', 'geopandas','movingpandas']
+)
+AS r"""
+from datetime import timedelta
+
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import movingpandas as mpd
+import json
+
+def main(traj_id, trajectory, min_angle, min_speed, min_length):
+    # build the DataFrame
+    df = pd.DataFrame.from_records(trajectory)
+
+    # build the GeoDataFrame
+    gdf = (
+      gpd.GeoDataFrame(
+        df[['t', 'properties']],
+        geometry=gpd.points_from_xy(df.lon, df.lat),
+        crs=4326
+      )
+      .set_index('t')
+    )
+
+    # build the Trajectory object
+    traj = mpd.Trajectory(gdf, traj_id)
+
+    result = mpd.AngleChangeSplitter(traj).split(
+        min_angle=min_angle,
+        min_speed=min_speed,
         min_length=min_length
     )
 
