c4d43019d880e104564ea314d9f5b400a44a2e79
Diego Vicente <dvicente@carto.com>
2025-05-08 14:17:31 +0200

    Include join mechanism in the component

--- components/trajectoryintersection/src/fullrun.sql
+++ components/trajectoryintersection/src/fullrun.sql
@@ -1,75 +1,49 @@
-IF return_polygon_properties THEN
-    EXECUTE IMMEDIATE FORMAT(
-        '''
-        CREATE OR REPLACE TABLE
-            `%s`
-        AS
-            WITH T AS(
-            SELECT ST_ASTEXT(%s) AS %s, 
-                %s
-            FROM `%s` 
-            )
-            SELECT
+EXECUTE IMMEDIATE FORMAT(
+    '''
+    CREATE OR REPLACE TABLE
+        `%s`
+    AS
+        WITH polygon_cte AS(
+        SELECT
+            * EXCEPT (%s),
+            ST_ASTEXT(%s) AS %s,
+        FROM `%s`
+        )
+        SELECT
+        %s,
+        @@workflows_temp@@.TRAJECTORY_INTERSECTION(
             %s,
-            @@workflows_temp@@.TRAJECTORY_INTERSECTION(
-                %s,
-                %s,
-                %s,
-                %s,
-                TRUE,
-                '%s'
-            ) AS %s
-            FROM `%s`
-            CROSS JOIN T
-        ''',
-        REPLACE(output_table, '`', ''),
-        polygon_col, polygon_col,
-        polygon_properties_col,
-        REPLACE(input_table_polygon, '`', ''),
-        traj_id_col,
-        traj_id_col,
-        tpoints_col,
-        polygon_col,
-        polygon_properties_col,
-        intersection_method,
-        tpoints_col,
-        REPLACE(input_table, '`', '')
-    );
-ELSE
-    EXECUTE IMMEDIATE FORMAT(
-        '''
-        CREATE OR REPLACE TABLE
-            `%s`
-        AS
-            WITH T AS(
-            SELECT ST_ASTEXT(%s) AS %s, 
-                %s
-            FROM `%s` 
-            )
-            SELECT
             %s,
-            @@workflows_temp@@.TRAJECTORY_INTERSECTION(
-                %s,
-                %s,
-                %s,
-                %s,
-                FALSE,
-                '%s'
-            ) AS %s
-            FROM `%s`
-            CROSS JOIN T
-        ''',
-        REPLACE(output_table, '`', ''),
-        polygon_col, polygon_col,
-        polygon_properties_col,
-        REPLACE(input_table_polygon, '`', ''),
-        traj_id_col,
-        traj_id_col,
-        tpoints_col,
-        polygon_col,
-        polygon_properties_col,
-        intersection_method,
-        tpoints_col,
-        REPLACE(input_table, '`', '')
-    );
-END IF;
+            %s,
+            %s,
+            %s,
+            '%s'
+        ) AS %s
+        FROM %s
+    ''',
+    REPLACE(output_table, '`', ''),
+    polygon_col,
+    polygon_col, polygon_col,
+    REPLACE(input_table_polygon, '`', ''),
+    traj_id_col,
+    traj_id_col,
+    tpoints_col,
+    polygon_col,
+    polygon_properties_col,
+    CASE WHEN return_polygon_properties THEN 'TRUE' ELSE 'FALSE' END,
+    intersection_method,
+    tpoints_col,
+    CASE WHEN join_type = 'Cross Join' THEN
+        FORMAT(
+            '`%s` t CROSS JOIN polygon_cte p',
+            REPLACE(input_table, '`', '')
+        )
+    WHEN join_type = 'Key Join' THEN
+        FORMAT(
+            '`%s` t INNER JOIN polygon_cte p ON t.%s = p.%s',
+            REPLACE(input_table, '`', ''),
+            traj_key_col,
+            polygon_key_col
+        )
+    END
+);
